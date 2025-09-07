4e0b6b627dec0d3a8d1aec5de2061b119fff532b
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-09-07 15:43:11 +0530

    added options in cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,41 +1,43 @@
-const jwt  = require("jsonwebtoken")
-const {sendError, sendSuccess} = require("../utils/response")
-const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken");
+const { sendError, sendSuccess } = require("../utils/response");
+const userModel = require("../models/user.model");
 
-module.exports.googleCallback=(req, res)=>{
-    
-    const token = jwt.sign({id:req.user._id}, process.env.JWT_SECRET)
-    res.cookie("token", token)
+module.exports.googleCallback = (req, res) => {
+  const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
 
-    res.redirect(`${process.env.CLIENT_URL}/auth/success?token=${token}`)
-}
+  res.redirect(`${process.env.CLIENT_URL}/auth/success?token=${token}`);
+};
 
-module.exports.logoutUser=(req, res)=>{
-    res.cookie("token", "");
-    req.userId = undefined
-    sendSuccess(res, 200,true, "Logout successfull", undefined)
-}
+module.exports.logoutUser = (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  req.userId = undefined;
+  sendSuccess(res, 200, true, "Logout successfull", undefined);
+};
 
-module.exports.getUser=async(req, res)=>{
-    const {id} = req.userId;
+module.exports.getUser = async (req, res) => {
+  const { id } = req.userId;
 
-    try{
-        const user = await userModel.findById(id);
-        if(!user) return sendError(
-          res,
-          404,
-          false,
-          "An error occurs while getting user.",
-          { message: 'User not found', code:404 }
-        );
+  try {
+    const user = await userModel.findById(id);
+    if (!user)
+      return sendError(res, 404, false, "An error occurs while getting user.", {
+        message: "User not found",
+        code: 404,
+      });
 
-        sendSuccess(res, 200, true, "Successfully get user", {user})
-
-    }catch(error){
-        sendError(res, 400, false, "An error occurs while getting user.", {error})
-    }
-
-}
-
-
-
+    sendSuccess(res, 200, true, "Successfully get user", { user });
+  } catch (error) {
+    sendError(res, 400, false, "An error occurs while getting user.", {
+      error,
+    });
+  }
+};
