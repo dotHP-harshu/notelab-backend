3a641974ae010d6935473f5be72779006dbc23a1
Harsh Prajapati <158311647+dotHP-harshu@users.noreply.github.com>
2025-09-13 10:57:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const databaseConnection = () => {
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI,{useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 15000})
     .then(() => console.log("db connected"))
     .catch((err) => {
       console.log(err);
